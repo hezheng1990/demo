ba36c25955967680b29c9f9a12a7c800e719d050
hezheng <hezheng@jtexpress.com>
2024-06-20 18:32:18 +0800

    文章功能

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Article;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.ArticleDao;
 import com.example.demo.mapper.UserDao;
@@ -25,65 +26,26 @@ import java.util.UUID;
 @RequestMapping("/article")
 public class ArticleController {
 
-
-    @PostMapping("/login")
-    public Result<String> login(@RequestBody User user) {
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<User> query = cb.createQuery(User.class);
-        Root<User> root = query.from(User.class);
-        query.where(cb.and(
-                cb.equal(root.get("name"), user.getName()),
-                cb.equal(root.get("password"), user.getPassword())
-        ));
-
-        List<User> userList = entityManager.createQuery(query).getResultList();
-
-        if(userList.size() > 0) {
-            String key = UUID.randomUUID().toString().replace("-","");
-            userMap.put(key,user);
-
-            Cookie cookie = new Cookie("x-header", key);
-            cookie.setPath("/");
-            cookie.setMaxAge(7*24*60*60); // 设置 Cookie 的过期时间，单位为秒
-            httpResponse.addCookie(cookie);
-
-            return Result.success(key);
-        }
+    @Autowired
+    private ArticleDao articleDao;
 
 
-        return Result.failed("failed");
+    @PostMapping("/add")
+    public Result add(@RequestBody Article article) {
+        articleDao.save(article);
+        return Result.success("写入成功");
+    }
+
+    @GetMapping("/selectAll")
+    public Result<List<Article>> selectAll() {
+        return Result.success(articleDao.findAll());
     }
 
 
-    @PostMapping("/registerOrLogin")
-    public Result<String> register(@RequestBody User user) {
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<User> query = cb.createQuery(User.class);
-        Root<User> root = query.from(User.class);
-        query.where(cb.and(
-                cb.equal(root.get("name"), user.getName())
-        ));
-
-        List<User> userList = entityManager.createQuery(query).getResultList();
-
-        if(userList.size() > 0) {
-            return login(user);
-        }else{
-
-            User user2 = userDao.save(user);
-            String key = UUID.randomUUID().toString().replace("-","");
-
-            userMap.put(key,user2);
-
-            Cookie cookie = new Cookie("x-header", key);
-            cookie.setMaxAge(7*24*60*60); // 设置 Cookie 的过期时间，单位为秒
-            httpResponse.addCookie(cookie);
-            return Result.success(key);
-        }
 
 
 
-    }
+
 
 
 }
